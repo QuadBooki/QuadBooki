1a35172375a3072d3b702cd59df90511feffdf1a
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-11-06 09:55:34 +0900

    1차 설정

---
+++ src/main/java/kobuki/quadbooki/QuadBookiApplication.java
@@ -0,0 +1,13 @@
+package kobuki.quadbooki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadBookiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadBookiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kobuki/quadbooki/QuadBookiApplicationTests.java
@@ -0,0 +1,13 @@
+package kobuki.quadbooki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadBookiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
